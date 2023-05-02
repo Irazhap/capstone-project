826b3f13e25c42695e8544e967b7878eeb5e6978
Irazhap Manapov <manapov_irazhap@icloud.com>
2023-05-02 09:23:35 +0200

    add path

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-import NowPlayinglist from "@/components/NowPlayinglist";
-import PopulaList from "@/components/Popularlist";
-import UpcomingMovies from "@/components/Upcomingmovies";
+import NowPlayinglist from "@/components/NowPlayingList";
+import PopulaList from "@/components/PopularList";
+import UpcomingMovies from "@/components/UpcomingMovies";
 import TopratedList from "@/components/TopRatedList";
 
 export default function HomePage() {
