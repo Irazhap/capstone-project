4bb1b4fedb8aedfe06d2e62f57afe2d44f5b1ce1
Irazhap Manapov <manapov_irazhap@icloud.com>
2023-05-07 12:57:20 +0200

    add carousel in MovieList.js

--- components/Card.js
+++ components/Card.js
@@ -4,9 +4,9 @@ import styled from "styled-components";
 const CardImage = styled(Image)`
   border-radius: 10px;
   margin: 10px;
-  box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.3);
-  width: 160px;
-  height: 230px;
+  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.3);
+  width: 140px;
+  height: 210px;
 `;
 
 const StyledParagraph = styled.p`
--- components/MovieList.js
+++ components/MovieList.js
@@ -1,24 +1,34 @@
 import styled from "styled-components";
 import Card from "./Card";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const StyledUl = styled.ul`
-  list-style-type: none;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
+const StyledSlider = styled(Slider)`
+  .slick-slide > li {
+    margin: 0 16px;
+  }
 `;
 
 export default function MovieList({ movies, title }) {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+  };
+
   return (
     <>
       <h2>{title}</h2>
-      <StyledUl>
+      <StyledSlider {...settings}>
         {movies.map((movie) => (
           <li key={movie.id}>
             <Card image={movie.image} year={movie.year} title={movie.title} />
           </li>
         ))}
-      </StyledUl>
+      </StyledSlider>
     </>
   );
 }
