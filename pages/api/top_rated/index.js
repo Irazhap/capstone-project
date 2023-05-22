127e2b1435bebbbd179ac8bfce557e48ca4ecddb
Irazhap Manapov <manapov_irazhap@icloud.com>
2023-05-22 12:01:50 +0200

    add arror in my api route, replaced my API key in the .env file with a new one

--- pages/api/top_rated/index.js
+++ pages/api/top_rated/index.js
@@ -1,8 +1,14 @@
 import fetcher from "@/services/fetch";
 
 export default async function handler(request, response) {
-  const { page } = request.query;
-  const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=${page}`;
-  const movieData = await fetcher(url);
-  response.status(200).json(movieData);
+  try {
+    const { page } = request.query;
+    const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=${page}`;
+    const movieData = await fetcher(url);
+    response.status(200).json(movieData);
+  } catch (error) {
+    response
+      .status(500)
+      .json({ error: "An error occurred while fetching the data" });
+  }
 }
