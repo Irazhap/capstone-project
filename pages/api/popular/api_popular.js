95b9a28a1c07579060eaa6a177080da8bb14e9a3
Irazhap Manapov <manapov_irazhap@icloud.com>
2023-05-22 09:13:58 +0200

    add services/fetcher/index.js

--- pages/api/popular/api_popular.js
+++ pages/api/popular/api_popular.js
@@ -1,8 +1,9 @@
+import fetcher from "@/pages/services/fetch";
+
 export default async function handler(request, response) {
   // const { page } = request.query;
-  const apiResponse = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
-  );
-  const movieData = await apiResponse.json();
-  response.status(200).json(movieData.results);
+  const { page } = request.query;
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=${page}`;
+  const movieData = await fetcher(url);
+  response.status(200).json(movieData);
 }
--- pages/api/upcoming/api_upcoming.js
+++ pages/api/upcoming/api_upcoming.js
@@ -1,7 +1,8 @@
+import fetcher from "@/pages/services/fetch";
+
 export default async function handler(request, response) {
-  const apiResponse = await fetch(
-    `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}&language=en-US&page=1`
-  );
-  const movieData = await apiResponse.json();
-  response.status(200).json(movieData.results);
+  const { page } = request.query;
+  const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}&language=en-US&page=${page}`;
+  const movieData = await fetcher(url);
+  response.status(200).json(movieData);
 }
