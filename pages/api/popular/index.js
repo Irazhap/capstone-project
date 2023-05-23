cb0e40975983ef423e8b92b807b5e5ca025737c6
Irazhap Manapov <manapov_irazhap@icloud.com>
2023-05-23 16:22:22 +0200

    some fix try

--- pages/api/popular/index.js
+++ pages/api/popular/index.js
@@ -2,6 +2,7 @@ import fetcher from "@/services/fetch";
 
 export default async function handler(request, response) {
   try {
+    const key = process.env.API_KEY;
     const { page } = request.query;
     const url = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=${page}`;
     const movieData = await fetcher(url);
